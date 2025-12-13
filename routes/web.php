5a104d9cce3b0e75cf7eef5d84bd316362d12752
msikkc21 <izzaaida21@gmail.com>
2025-12-13 15:20:20 +0700

    feat: add contact page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/portfolio', function () {
     return view('LandingPage.Portfolio');
 });
 
+Route::get('/contact', function () {
+    return view('LandingPage.Contact');
+});
+
 Route::get('/welcome', function () {
     return view('welcome');
 });
