e8c3c27fe3320b1bf3797d3bce64aff093117a2e
msikkc21 <izzaaida21@gmail.com>
2025-12-14 21:10:26 +0700

    feat: add detail porto page

---
+++ resources/views/LandingPage/Component/CTASection.blade.php
@@ -0,0 +1,12 @@
+<section class="max-w-6xl mx-auto px-6 mb-20">
+    <div class="bg-secondary mx-auto px-6 py-16 flex flex-col gap-5 items-center justify-center rounded-xl ">
+        <h3 class="text-white font-bold text-3xl text-center">
+            Tertarik dengan Proyek Serupa?
+        </h3>
+        <p class="text-white text-lg ml-4 text-center">Mari diskusikan kebutuhan otomasi industri Anda dan temukan solusi terbaik bersama tim ahli kami.</p>
+        <div class="mt-3 flex flex-col md:flex-row gap-4">
+            <a href="" class="text-secondary font-semibold bg-white px-6 py-4 rounded-lg text-center">Konsultasi Gratis</a>
+            <a href="" class="text-white font-semibold bg-transparent border-2 border-white px-6 py-4 rounded-lg text-center">Lihat Portfolio Lain</a>
+        </div>
+    </div>
+</section>
