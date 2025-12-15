be8d2023b5871d0582027bca00eab54852312aa0
msikkc21 <izzaaida21@gmail.com>
2025-12-15 23:55:49 +0700

    fix: fix bug header

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -10,12 +10,11 @@
 <body>
     <section class="flex">
         @include('Admin.Component.Sidebar')
-        <main class="flex-1 py-4 min-h-screen md:pl-0">
+        <main class="flex-1 min-h-screen md:pl-0">
             @include('Admin.Component.Header', ['title' => $title ?? 'Dashboard Admin'])
 
-            <div class="px-5 pt-6 md:pl-72">
+            <div class="px-5 pt-24 md:pl-72 bg-gray-100 min-h-screen">
                 <h1>test</h1>
-                <!-- Konten dashboard dapat ditambahkan di sini -->
             </div>
         </main>
     </section>
