d7f4373e8978ac5aaff22f25179ede24a256f74b
msikkc21 <izzaaida21@gmail.com>
2025-12-15 23:48:12 +0700

    feat: add header on admin panel

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-    @php($title = 'Dashboard Admin')
+    @php
+    $title = 'Dashboard Admin';
+    $active = 'dashboard';
+    $slot = 5; // dummy variable to avoid blade error
+    @endphp
+
     @include('Component.Head')
 <body>
-    @php($active = 'dashboard')
-    @include('Admin.Component.Sidebar')
+    <section class="flex">
+        @include('Admin.Component.Sidebar')
+        <main class="flex-1 py-4 min-h-screen md:pl-0">
+            @include('Admin.Component.Header', ['title' => $title ?? 'Dashboard Admin'])
+
+            <div class="px-5 pt-6 md:pl-72">
+                <h1>test</h1>
+                <!-- Konten dashboard dapat ditambahkan di sini -->
+            </div>
+        </main>
+    </section>
 </body>
 </html>
