cbcd5b82dd6e71809aa802a4855921e3488aa5b4
Fadhlu Ibnu <f.ibnu.a1@gmail.com>
2026-01-20 15:42:37 +0700

    feat: active and inactive section

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\SiteSetting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
@@ -14,7 +15,9 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('auth.login');
+        return view('auth.login', [
+            'settings' => SiteSetting::allAsArray(),
+        ]);
     }
 
     /**
